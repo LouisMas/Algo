9387d2f23c9d91112ecd7c1c50a6690c90538f02
LouisMas <104148167+LouisMas@users.noreply.github.com>
2022-05-06 16:40:54 +0200

    Add files via upload

---
+++ src/Exo1/fr/exo01/Bienvenue.java
@@ -0,0 +1,19 @@
+package Exo1.fr.exo01;
+
+
+import java.util.Scanner;
+
+public class Bienvenue {
+
+    public static void i () {
+
+        String username;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrez votre nom");
+            username = scanner.next();
+
+
+        System.out.printf("Bienvenue " + username);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Exo1.fr.exo01.Bienvenue;
+import Exo2.fr.exo2.Addition;
+
+public class Main {
+    public static void main(String[] args) {
+        new Addition();
+
+    }
+}
