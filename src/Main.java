b87f143bfce76084e620060dccc15c5d0a72a823
Masurel Louis <mangaman77+>
2022-05-09 14:58:48 +0200

    Exo d'algo

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Exo2.fr.Addition;
+
+public class Main {
+    public static void main(String[] args) {
+        new Addition();
+
+    }
+}
---
+++ src/exo7/fr/Somme.java
@@ -0,0 +1,26 @@
+package exo7.fr;
+
+import java.util.Scanner;
+
+public class Somme {
+
+    public static void main(String[] args) {
+        
+        int nombre;
+        int resultat = 0;
+        Scanner scanNb = new Scanner(System.in);
+        System.out.println("Entrez votre chiffre : ");
+        nombre = scanNb.nextInt();
+        
+        for (int i = 1; i <=nombre; i++) {
+            resultat += i;
+            if (i != nombre) {
+                System.out.printf(i + " + ");
+            } else {
+                System.out.println(i + " = ");
+            }
+
+        }
+        System.out.printf(resultat);
+    }
+}
